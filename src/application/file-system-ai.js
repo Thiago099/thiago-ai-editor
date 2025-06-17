ce41d224098023ef166d65aeb67142eb0c7c9786
Thiago099 <Thiago099@users.noreply.github.com>
2025-06-17 14:43:35 -0300

    updated prompt structure

--- src/application/file-system-ai.js
+++ src/application/file-system-ai.js
@@ -2,14 +2,8 @@ import { Folder } from "../lib/folder.js"
 import { LLM } from "../lib/llm.js"
 
 const system = `
-You will receive zero or more <file> tags (each representing a complete file), and one <task> tag describing the task you must perform.
-<file filePath="The path to the file">
-    The full contents of the file
-</file>
-<task>
-    Instructions describing the actions to perform, possibly involving the provided files or the creation of new ones.
-</task>
-You must reply with the new files or the full new contents of the current files
+The user will provide you with the current state of the file system <fileSystem></fileSystem> and a <task></task>
+you must modify the files in order to do what the user wants
 `
 const schema = {
     'name': 'file_system',
@@ -21,7 +15,7 @@ const schema = {
             'properties': {
                 'filePath': {
                     'type': 'string',
-                    'description': 'The path of the modified or created file'
+                    'description': 'The path inside the project folder of the modified or created file'
                 },
                 'fileContent': {
                     'type': 'string',
@@ -41,8 +35,7 @@ class FileSystemAI{
             const llm = new LLM(system, url)
 
             llm["json_schema"] = schema
-
-            llm.User(`${Folder.FromJson(ctx)}\n<task>\n${task}\n</task>`)
+            llm.User(`<fileSystem>${Folder.FromJson(ctx)}</fileSystem>\n<task>\n${task}\n</task>`)
             const message = await llm.Continue()
             if(message == null){
                 return message
